426f1db51f47874c9ad0cfad6d57c0bd72c5dc4f
Skillkiller <seblsebastian@aol.de>
2023-05-26 22:29:07 +0200

    Update to meet server requirements

--- step5.sh
+++ step5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 input_dir="clips-sorted"
 outdir_dir="json"
-chunk_size=2
-model="tiny"
-device="cpu"
+chunk_size=25
+model="large"
+device="cuda"
 source="test"
 
 mkdir -p "$outdir_dir"
@@ -19,7 +19,7 @@ for folder in $(find "$input_dir" -maxdepth 1 -type d); do
         files=$(find "$folder" -maxdepth 1 -type f -name "*.m4a" -printf "../%h/%f\n" | head -n "$chunk_size")
         #find "$source_folder" -maxdepth 1 -type f | head -n "$chunk_size" | xargs -I{} mv {} "$destination_folder"
         pushd "whisper-tmp"
-        echo "$files" | xargs whisper --model "$model" --device "$device" --language "$sprache"
+        echo "$files" | xargs whisper --model "$model" --device "$device" --language "$sprache" > /dev/null
         mkdir -p "../$folder/finished/"
         echo "$files" | xargs -I{} mv {} "../$folder/finished"
         popd
@@ -28,8 +28,8 @@ for folder in $(find "$input_dir" -maxdepth 1 -type d); do
         for file in $(find -maxdepth 1 -type f -name "*.txt"); do
         echo "$file"
             # Erzeuge json
-            output_file="../$outdir_dir/$(echo "$file" | sed 's/m4a\.txt/json/')"
-            "C:\Users\sebls\Downloads\jq.exe" -n --arg model "$model" --arg text "$(cat $file)" --arg source "$source" '{ model: $model, text: $text, source: $source }' > "$output_file"
+            output_file="../$outdir_dir/$(echo "$file" | sed 's/txt/json/')"
+            jq -n --arg model "$model" --arg text "$(cat $file)" --arg source "$source" '{ model: $model, text: $text, source: $source }' > "$output_file"
             sed -i 's/\\r//g' "$output_file"
         done
         # Lösche nicht benötigte whisper output Dateien
@@ -37,6 +37,7 @@ for folder in $(find "$input_dir" -maxdepth 1 -type d); do
         rm *.txt
         rm *.vtt
         rm *.json
+        rm *.tsv
         popd
     done
 
