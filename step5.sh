364f7f22dd54d31f4ca4e8d5ab6f8abea93f2b59
Skillkiller <seblsebastian@aol.de>
2023-05-26 22:55:23 +0200

    Add status echos

--- step5.sh
+++ step5.sh
@@ -8,30 +8,32 @@ source="test"
 
 mkdir -p "$outdir_dir"
 mkdir -p "whisper-tmp"
-
+file_counter=0
 for folder in $(find "$input_dir" -maxdepth 1 -type d); do
   if [[ "$folder" != "$input_dir" ]]; then
     sprache=$(basename "$folder")
 
 
     while [[ $(find "$folder" -maxdepth 1 -type f | wc -l) -gt 0 ]]; do
-    # Verschieben der Dateien in den Zielordner
         files=$(find "$folder" -maxdepth 1 -type f -name "*.m4a" -printf "../%h/%f\n" | head -n "$chunk_size")
-        #find "$source_folder" -maxdepth 1 -type f | head -n "$chunk_size" | xargs -I{} mv {} "$destination_folder"
         pushd "whisper-tmp"
+        echo "Start transcribing."
         echo "$files" | xargs whisper --model "$model" --device "$device" --language "$sprache" > /dev/null
         mkdir -p "../$folder/finished/"
         echo "$files" | xargs -I{} mv {} "../$folder/finished"
         popd
+
+        file_counter=$((file_counter + $chunk_size))
+        echo "Transcribed $chunk_size audios. Total $file_counter files processed."
         
         pushd "whisper-tmp"
         for file in $(find -maxdepth 1 -type f -name "*.txt"); do
-        echo "$file"
             # Erzeuge json
             output_file="../$outdir_dir/$(echo "$file" | sed 's/txt/json/')"
             jq -n --arg model "$model" --arg text "$(cat $file)" --arg source "$source" '{ model: $model, text: $text, source: $source }' > "$output_file"
             sed -i 's/\\r//g' "$output_file"
         done
+        echo "Generated json files"
         # Lösche nicht benötigte whisper output Dateien
         rm *.srt
         rm *.txt
